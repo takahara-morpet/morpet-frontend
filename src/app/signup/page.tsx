d38dc83ad1b3b27acfcce5b7f795c359355eb5db
takaharayota531 <takahara.yota@gmail.com>
2024-09-21 18:41:02 +0900

    signupページの実装

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,16 +1,56 @@
+"use client";
 import SubmitButton from "@/components/template/SubmitButton";
-import { sign } from "chart.js/helpers"
+import { createUser } from "@/lib/api/user";
+import { UserCreateRequest } from "@/types/request/user";
+import { sign } from "chart.js/helpers";
+import React, { useState } from "react";
+import SignUpForm from "@/components/template/SignUpForm";
 
+const SignUpPage: React.FC = () => {
+  const [formData, setFormData] = useState<UserCreateRequest>({
+    age: 0,
+    name: "",
+    profile: "",
+    profileImageUrl: "/images/suga.jpg",
+    gender: "",
+    mbti: "",
+  });
+  const [error, setError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
- const SignUpPage: React.FC = () => {
-    return <div>
-        hello
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await createUser(formData);
+      setSuccessMessage("ユーザー登録が成功しました！");
+      setError(null);
+    } catch (err) {
+      console.log("Error creating user:", err);
+      setError("ユーザー登録に失敗しました。");
+      setSuccessMessage(null);
+    }
+  };
 
-        <div className="fixed -bottom-80 right-80 z-50">
-        <SubmitButton />
-      </div>
-    </div>;
-}
+  return (
+    <SignUpForm
+      formData={formData}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      error={error}
+      successMessage={successMessage}
+    />
+  );
+};
 
 export default SignUpPage;
