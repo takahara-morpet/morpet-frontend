5f33df9b1c4405b9d8ebe4f147f93c2d553d1f57
takaharayota531 <takahara.yota@gmail.com>
2024-09-23 16:41:27 +0900

    deploy fix

--- src/lib/api/reply.tsx
+++ src/lib/api/reply.tsx
@@ -1,12 +1,12 @@
-import { POSTS_URL, REPLYS_URL } from "@/constants/routes";
+import { REPLIES_URL } from "@/constants/routes";
 import { ReplyCreateRequest } from "@/types/request/reply";
 import { ReplyGetResponse } from "@/types/response/reply";
 import axios from "axios";
 
 export const fetchReplies = async (): Promise<ReplyGetResponse[]> => {
   try {
-    console.log(REPLYS_URL);
-    const response = await axios.get(REPLYS_URL);
+    console.log(REPLIES_URL);
+    const response = await axios.get(REPLIES_URL);
     return response.data;
   } catch (error) {
     console.log("Error fetching reply:", error);
@@ -17,7 +17,7 @@ export const fetchReplies = async (): Promise<ReplyGetResponse[]> => {
 // post_id に基づいて返信を取得する関数
 export const fetchRepliesById = async (postId: string): Promise<ReplyGetResponse[]> => {
   try {
-    const response = await axios.get(`${REPLYS_URL}/${postId}`);
+    const response = await axios.get(`${REPLIES_URL}/${postId}`);
     return response.data;
   } catch (error) {
     console.log(`Error fetching replies for postId ${postId}:`, error);
@@ -29,8 +29,8 @@ export const createReply = async (
   replyData: ReplyCreateRequest
 ): Promise<[]> => {
   try {
-    console.log("Sending POST request to:", REPLYS_URL);
-    const response = await axios.post(REPLYS_URL, replyData);
+    console.log("Sending POST request to:", REPLIES_URL);
+    const response = await axios.post(REPLIES_URL, replyData);
     console.log("Response data:", response.data);
     return response.data;
   } catch (error) {
