72d1aafe284e5256cadddd43b0227d8ea76fbace
takaharayota531 <takahara.yota@gmail.com>
2024-09-25 19:12:51 +0900

    post

--- src/components/modules/GenderBar.tsx
+++ src/components/modules/GenderBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { UserIcon } from "@heroicons/react/24/outline";
 import { motion, AnimatePresence } from "framer-motion";
 import "./GenderBar.css";
