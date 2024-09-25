c6b6ed73937ab10c063ee6a80fc1d195da0936f9
takaharayota531 <takahara.yota@gmail.com>
2024-09-25 13:39:00 +0900

    user signup navigationhttps://u-tokyo-ac-jp.zoom.us/j/86457405657?pwd=FUoTu0JaRUzOC6EhoRsRsaVVQHY5bT.1

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,25 +1,49 @@
+"use client";
 // components/Footer.tsx
 import React from "react";
 import Link from "next/link";
 import "./Footer.css";
-import { HomeIcon, BellIcon, UserIcon, EnvelopeIcon } from '@heroicons/react/24/outline'; // Heroiconsをインポート
+import {
+  HomeIcon,
+  BellIcon,
+  UserIcon,
+  EnvelopeIcon,
+  UsersIcon,
+} from "@heroicons/react/24/outline"; // Heroiconsをインポート
 
 const Footer: React.FC = () => {
+  const userId = localStorage.getItem("userId");
+
   return (
     <footer className="footer">
       <ul className="navLinks">
-        <li><Link href="/timeline">
-          <HomeIcon className="icon" />
-        </Link></li>
-        <li><Link href="/profile">
-          <UserIcon className="icon" />
-        </Link></li>
-        <li><Link href="/notifications">
-          <BellIcon className="icon" />
-        </Link></li>
-        <li><Link href="/message">
-          <EnvelopeIcon className="icon" />
-        </Link></li>
+        <li>
+          <Link href="/timeline">
+            <HomeIcon className="icon" />
+          </Link>
+        </li>
+        <li>
+          <Link href="/profile">
+            <UsersIcon className="icon" />
+          </Link>
+        </li>
+        <li>
+          <Link href="/notifications">
+            <BellIcon className="icon" />
+          </Link>
+        </li>
+        <li>
+          <Link href="/message">
+            <EnvelopeIcon className="icon" />
+          </Link>
+        </li>
+        {userId && (
+          <li>
+            <Link href={`/profile/${userId}`}>
+              <UserIcon className="icon" />
+            </Link>
+          </li>
+        )}
       </ul>
     </footer>
   );
