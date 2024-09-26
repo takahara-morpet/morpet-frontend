b6d23cee9c042c04dfa6272eb3469537183464bd
koki-ota0 <kokiota2000@gmail.com>
2024-09-26 18:29:33 +0900

    デモデータの作成

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -10,24 +10,34 @@ const Notifications: React.FC = () => {
       <h1 className="hedder">通知</h1>
       <ul className="notification-list">
         <li className="notification-item">
-          <Image src="/images/lion.jpg" alt="lion" className="avatar" width={40} height={40} />
-          <strong>らいおん</strong> があなたの投稿にいいねしました！
-          <span className="timestamp">2024-09-25 12:30</span>
+          <Image src="/images/penguin.jpg" alt="penguin" className="avatar" width={40} height={40} />
+          <strong>メンヘラぺんぎん</strong> があなたの投稿にいいねしました！
+          <span className="timestamp">2024-09-25 00:30</span>
         </li>
         <li className="notification-item">
           <Image src="/images/penguin.jpg" alt="penguin" className="avatar" width={40} height={40} />
-          <strong>ぺんぎん</strong> があなたにメッセージを送りました。
-          <span className="timestamp">2024-09-24 15:45</span>
+          <strong>メンヘラぺんぎん</strong> があなたにメッセージを送りました。
+          <span className="timestamp">2024-09-24 23:45</span>
         </li>
         <li className="notification-item">
           <Image src="/images/elephant.jpg" alt="elephant" className="avatar" width={40} height={40} />
-          <strong>ぞう</strong> があなたをフォローしました。
+          <strong>ひもになりたい</strong> があなたをフォローしました。
           <span className="timestamp">2024-09-23 18:00</span>
         </li>
         <li className="notification-item">
-          <Image src="/images/crab.jpg" alt="fox" className="avatar" width={40} height={40} />
-          <strong>かに</strong> があなたの投稿にコメントしました。
+          <Image src="/images/penguin.jpg" alt="penguin" className="avatar" width={40} height={40} />
+          <strong>メンヘラぺんぎん</strong> があなたの投稿にいいねしました！
+          <span className="timestamp">2024-09-23 01:30</span>
+        </li>
+        <li className="notification-item">
+          <Image src="/images/azarashi.jpg" alt="azarashi" className="avatar" width={40} height={40} />
+          <strong>応援団長</strong> があなたの投稿にコメントしました。
           <span className="timestamp">2024-09-22 20:15</span>
+        </li>
+        <li className="notification-item">
+          <Image src="/images/penguin.jpg" alt="penguin" className="avatar" width={40} height={40} />
+          <strong>メンヘラぺんぎん</strong> があなたの投稿にいいねしました！
+          <span className="timestamp">2024-09-22 03:00</span>
         </li>
       </ul>
     </div>
